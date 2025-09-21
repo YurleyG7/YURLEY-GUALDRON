ebdf2c8d7b0ef2f565ce0ceae5c7c7a04794feb2
Yurley Gualdron <gualdronyurley7@gmail.com>
2025-09-20 23:10:12 -0500

    Creacion Post y Delete en el modulo Client

--- primera-app/src/client/client.controller.ts
+++ primera-app/src/client/client.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Delete, Param, Post } from '@nestjs/common';
 
 @Controller('client')
 export class ClientController {
@@ -7,5 +7,13 @@ export class ClientController {
     askForCoffee() {
         return 'Se solicita un café';
     }
-
+    @Delete("/:id/:name")
+    deleteSalesRecord(@Param('id') id:number, @Param('name') name:string ) {
+        return (
+            'Se elimino el registro del café con id' + 
+            id + 
+            'y se pauso la venta:' +
+            name
+        );
+    }
 }
